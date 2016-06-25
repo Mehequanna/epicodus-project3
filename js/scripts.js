e829ffdd81bcd184207afd7dd0eff97d5f2f6f6b
Stephen Emery <emery.stephen@gmail.com>
2016-06-24 20:57:06 -0700

    Add business logic of pingpong output.
    Took a few tries but finally got it :/

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,27 @@
 // Business End Logic
 
+  function pingPong(input) {
+    for (var i=0; i <= input; i+=1) {
+      if(i % 15 === 0) {
+        console.log("pingpong");
+      } else if (i % 3 === 0) {
+        console.log("ping");
+      } else if (i % 5 === 0) {
+        console.log("pong");
+      } else {
+        console.log(i);
+      }
+    }
+  }
 
 // User End Logic
 $(document).ready(function() {
   $("form#numberForm").submit(function(event) {
+    var input = parseInt($("input#numberInput").val());
+    pingPong(input);
 
     event.preventDefault();
   });
 });
+
+// $("ul#numberOutput").append("<li>pingpong</li>");

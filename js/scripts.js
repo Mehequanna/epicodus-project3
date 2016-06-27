b071b7f3f5a0137bf4cf657b62143d248e983be2
Stephen Emery <emery.stephen@gmail.com>
2016-06-27 15:05:19 -0700

    Make the function pingPong print to the output array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 // Business End Logic
+var output = [];
 
-  function pingPong(input) {
-    for (var i=1; i <= input; i+=1) {
-      if(i % 15 === 0) {
-        $("ul#numberOutput").append("<li>pingpong</li>");
-      } else if (i % 3 === 0) {
-        $("ul#numberOutput").append("<li>ping</li>");
-      } else if (i % 5 === 0) {
-        $("ul#numberOutput").append("<li>pong</li>");
-      } else {
-        $("ul#numberOutput").append("<li>" + i + "</li>");
-      }
+function pingPong(input) {
+  for (var i=1; i <= input; i+=1) {
+    if(i % 15 === 0) {
+      output.push("pingpong");
+    } else if (i % 3 === 0) {
+      output.push("ping");
+    } else if (i % 5 === 0) {
+      output.push("pong");
+    } else {
+      output.push(i);
     }
   }
+}
+
 
 // User End Logic
 $(document).ready(function() {
